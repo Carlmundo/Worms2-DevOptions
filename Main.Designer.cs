d19279bcdda558295728eb2bb825a38a4fde5206
Carl Norton <Carlmundo@users.noreply.github.com>
2024-12-07 21:17:52 +0000

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace W2_DevMode
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.lblStatus, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnPatch, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(337, 121);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblStatus.Location = new System.Drawing.Point(4, 16);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 16, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(329, 29);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPatch.AutoSize = true;
+            this.btnPatch.Location = new System.Drawing.Point(71, 60);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(194, 45);
+            this.btnPatch.TabIndex = 1;
+            this.btnPatch.Text = "Patch";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(337, 121);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.857143F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnPatch;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace W2_DevMode
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public static class options
+        {
+            public static string filename = "frontend.exe";
+            public static bool patched = false;    
+            public static Dictionary<long, byte> plus = new Dictionary<long, byte>
+            {
+                //Options
+                {0x58307, 0x64}, //Crate drops (%)
+                {0x5832E, 0x64}, //Crate intelligence (%)
+                {0x58355, 0x7D}, //First aid crates (HP)
+                {0x57F3E, 0x78}, //Round Time (minutes)
+                {0x57F64, 0x78}, //Turn Time (seconds)
+                {0x57F66, 0x1}, //Turn Time (seconds) - Minimum
+                {0x57F8A, 0x3C}, //Worm retreat (seconds)
+                {0x57FB0, 0x3C}, //Rope retreat (seconds)
+                {0x58275, 0x78}, //Delay between turns (seconds)
+                {0x57FD6, 0x18}, //Landmines
+                {0x57FFC, 0xA}, //Mine explosion delay (seconds)
+                {0x5819F, 0x64}, //Repeat Swings
+                {0x581A1, 0xFF}, //Repeat Swings - Minimum
+                {0x581C6, 0x7D}, //Fall Damage
+                {0x5829C, 0xF}, //Worms starting energy (Byte 1)
+                {0x5829D, 0x27}, //Worms starting energy (Byte 2)
+                {0x582A1, 0x1}, //Worms starting energy - Minimum
+                {0x5820D, 0x7F}, //Land Sink Rate
+                {0x5820F, 0xFF}, //Land Sink Rate - Minimum
+                {0x580EC, 0x12}, //Auto Replay Kills
+                {0x58113, 0x10}, //Auto Replay Damage (Byte 1)
+                {0x58114, 0x27}, //Auto Replay Damage (Byte 2)
+                {0x58084, 0x7D}, //Wind Strength
+
+                //Weapons
+                {0x1170B8, 0x64}, //Stock
+                {0x1170C4, 0x64}, //Weapon delay
+                {0x1170D0, 0x3C}, //Retreat time
+                {0x1170DC, 0x64}, //Total ammo in a crate
+                {0x1170E8, 0xFF}, //Bullet count
+                {0x1170F4, 0x64}, //Appears in crates (%)
+                {0x117100, 0xFF}, //Weapon damage
+                {0x11710C, 0xFF}, //Blast power
+                {0x117118, 0xFF}, //Explosion bias
+                {0x117124, 0x30}, //Homing delay (milliseconds) (Byte 1)
+                {0x117125, 0x75}, //Homing delay (milliseconds) (Byte 2)
+                {0x117130, 0x30}, //Homing time (milliseconds) (Byte 1)
+                {0x117131, 0x75}, //Homing time (milliseconds) (Byte 2)
+                {0x11713C, 0xFF}, //Wind response
+                {0x117148, 0xFF}, //No. clusters
+                {0x117154, 0xFF}, //Cluster power (Byte 1)
+                {0x117155, 0x0}, //Cluster power (Byte 2)
+                {0x117160, 0xFF}, //Cluster angle
+                {0x11716C, 0xFF}, //Cluster damage
+                {0x117178, 0xA}, //Override fuse
+                {0x117184, 0x24}, //Amount of fire
+                {0x117190, 0xFF}, //Fire spread speed
+                {0x11719C, 0xFF}, //Fire period
+                {0x1171A8, 0xFF}, //Impact force
+                {0x1171B4, 0xFF}, //Impact angle
+                {0x1171C0, 0xFF}, //Impact damage
+                {0x1171CC, 0xFF}, //Jump height
+                {0x1171D8, 0xFF}, //Ball damage
+                {0x1171E4, 0xFF}, //Power of ball impact
+                {0x1171F0, 0xFF}, //Angle of ball impact
+                {0x1171FC, 0x30}, //Time of ball (Byte 1)
+                {0x1171FD, 0x75}, //Time of ball (Byte 2)
+                {0x117208, 0x30}, //Dig time (Byte 1)
+                {0x117209, 0x75}, //Dig time (Byte 2)
+                {0x117214, 0xFF}, //Bomblets
+                {0x117220, 0xFF}, //Bomb damage
+                {0x11722C, 0xFF}, //Bullet spread
+            };
+            public static Dictionary<long, byte> dev = new Dictionary<long, byte>
+            {
+                //Options
+                {0x58307, 0x64}, //Crate drops (%)
+                {0x5832E, 0x64}, //Crate intelligence (%)
+                {0x58355, 0x7F}, //First aid crates (HP)
+                {0x57F3E, 0x7F}, //Round Time (minutes)
+                {0x57F64, 0x7F}, //Turn Time (seconds)
+                {0x57F66, 0x1}, //Turn Time (seconds) - Minimum
+                {0x57F8A, 0x7F}, //Worm retreat (seconds)
+                {0x57FB0, 0x7F}, //Rope retreat (seconds)
+                {0x58275, 0x7F}, //Delay between turns (seconds)
+                {0x57FD6, 0x18}, //Landmines
+                {0x57FFC, 0x7F}, //Mine explosion delay (seconds)
+                {0x5819F, 0x7F}, //Repeat Swings
+                {0x581A1, 0xFF}, //Repeat Swings - Minimum
+                {0x581C6, 0x7F}, //Fall Damage
+                {0x5829C, 0xF}, //Worms starting energy (Byte 1)
+                {0x5829D, 0x27}, //Worms starting energy (Byte 2)
+                {0x582A1, 0x1}, //Worms starting energy - Minimum
+                {0x5820D, 0x7F}, //Land Sink Rate
+                {0x5820F, 0xFF}, //Land Sink Rate - Minimum
+                {0x580EC, 0x12}, //Auto Replay Kills
+                {0x58113, 0xFF}, //Auto Replay Damage (Byte 1)
+                {0x58114, 0xFF}, //Auto Replay Damage (Byte 2)
+                {0x58084, 0x7F}, //Wind Strength
+
+                //Weapons
+                {0x1170B8, 0xFF}, //Stock
+                {0x1170C4, 0xFF}, //Weapon delay
+                {0x1170D0, 0xFF}, //Retreat time
+                {0x1170DC, 0xFF}, //Total ammo in a crate
+                {0x1170E8, 0xFF}, //Bullet count
+                {0x1170F4, 0x64}, //Appears in crates (%)
+                {0x117100, 0xFF}, //Weapon damage
+                {0x11710C, 0xFF}, //Blast power
+                {0x117118, 0xFF}, //Explosion bias
+                {0x117124, 0xFF}, //Homing delay (milliseconds) (Byte 1)
+                {0x117125, 0xFF}, //Homing delay (milliseconds) (Byte 2)
+                {0x117130, 0xFF}, //Homing time (milliseconds) (Byte 1)
+                {0x117131, 0xFF}, //Homing time (milliseconds) (Byte 2)
+                {0x11713C, 0xFF}, //Wind response
+                {0x117148, 0xFF}, //No. clusters
+                {0x117154, 0xFF}, //Cluster power (Byte 1)
+                {0x117155, 0x0}, //Cluster power (Byte 2)
+                {0x117160, 0xFF}, //Cluster angle
+                {0x11716C, 0xFF}, //Cluster damage
+                {0x117178, 0xFF}, //Override fuse
+                {0x117184, 0xFF}, //Amount of fire
+                {0x117190, 0xFF}, //Fire spread speed
+                {0x11719C, 0xFF}, //Fire period
+                {0x1171A8, 0xFF}, //Impact force
+                {0x1171B4, 0xFF}, //Impact angle
+                {0x1171C0, 0xFF}, //Impact damage
+                {0x1171CC, 0xFF}, //Jump height
+                {0x1171D8, 0xFF}, //Ball damage
+                {0x1171E4, 0xFF}, //Power of ball impact
+                {0x1171F0, 0xFF}, //Angle of ball impact
+                {0x1171FC, 0xFF}, //Time of ball (Byte 1)
+                {0x1171FD, 0xFF}, //Time of ball (Byte 2)
+                {0x117208, 0xFF}, //Dig time (Byte 1)
+                {0x117209, 0xFF}, //Dig time (Byte 2)
+                {0x117214, 0xFF}, //Bomblets
+                {0x117220, 0xFF}, //Bomb damage
+                {0x11722C, 0xFF}, //Bullet spread
+            };
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            try {
+                checkIfPatched();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error");
+                Application.Exit();
+            }
+            
+        }
+        private void checkIfPatched()
+        {
+            using (FileStream fsSource = new FileStream(options.filename, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binaryReader = new BinaryReader(fsSource)) {
+                fsSource.Seek(0x58355, SeekOrigin.Begin);
+                ushort result = binaryReader.ReadByte();
+                if (result.ToString() == "125") { //Check value of First aid crate
+                    options.patched = false;
+                    lblStatus.Text = "Frontend is unpatched.";
+                    btnPatch.Text = "Patch";
+                }
+                else {
+                    options.patched = true;
+                    lblStatus.Text = "Frontend is patched.";
+                    btnPatch.Text = "Reverse Patch";
+                }
+            }
+        }
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+            Dictionary<long, byte> setOptions = new Dictionary<long, byte> { };
+            if (options.patched) {
+                setOptions = options.plus;
+            }
+            else {
+                setOptions = options.dev;
+            }
+            try {
+                Stream outStream = File.Open(options.filename, FileMode.Open);
+                foreach (KeyValuePair<long, byte> setting in setOptions) {
+                    outStream.Seek(setting.Key, SeekOrigin.Begin);
+                    outStream.WriteByte(setting.Value);
+                }
+                outStream.Close();
+                checkIfPatched();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message,"Error");
+            }
+        }
+    }
+}
